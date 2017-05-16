e42beb5dce3ab0430f45f5f459243c6bd02fc7c4
LoganK64 <SirloganK@gmail.com>
2017-05-16 09:35:03 -0500

    Update MAIN.java

--- MAIN.java
+++ MAIN.java
@@ -18,15 +18,4 @@ public class MAIN
         things = new ArrayList();
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
-    }
 }
