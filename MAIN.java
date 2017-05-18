fd6f0746339bd0e728a082938c4de9ddcb3bb17e
LoganK64 <sirlogank@gmail.com>
2017-05-18 10:35:34 -0500

    Removed all the bad comments

--- MAIN.java
+++ MAIN.java
@@ -6,50 +6,22 @@ import java.util.*;
  */
 public class MAIN
 {
-<<<<<<< HEAD
     private ArrayList<Result> things;
-=======
-    // instance variables - replace the example below with your own
-    private ArrayList<Result> things;
-
-    /**
-     * Constructor for objects of class MAIN
-     */
->>>>>>> refs/remotes/origin/master
     public MAIN()
     {
         things = new ArrayList();
     }
-<<<<<<< HEAD
-=======
-
->>>>>>> refs/remotes/origin/master
     public void add(int id, String name, int value)
     {
         Result thing = new Result(id,name,value);
         things.add(thing);
     }
-<<<<<<< HEAD
 	public void remove(int index)
 	{
 		things.remove(index);
-	}
-=======
-    public void remove(int index)
-    {
-        things.remove(index);
-        
-    }
-    
+	}    
     public Result searchByName(String key)
     {
-        /*for (int i = 0; i < things.size(); i++)
-        {
-            if (key.equals(things.get(i).getName()))
-            {
-                return i;
-            }
-        } */
         for(Result i: things)
         {
             if(key.equals(i.getName()))
@@ -58,5 +30,4 @@ public class MAIN
         }
         return null;
        }
->>>>>>> refs/remotes/origin/master
 }
