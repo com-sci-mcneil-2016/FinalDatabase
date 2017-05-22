0324e56fd2b4e338099bfb58fab6417e537d5228
kyle <kyle.dodds.2013@gmail.com>
2017-05-22 10:13:04 -0500

    fix the gooy

---
+++ SearchButton.java
@@ -0,0 +1,16 @@
+import mayflower.*;
+public class SearchButton extends Actor
+{
+    public SearchButton()
+    {
+        setImage("img/playagain.png");
+    }
+    @Override
+    public void act()
+    {
+        if( Mayflower.mouseClicked(this) )
+        {
+            
+        }
+    }
+}
